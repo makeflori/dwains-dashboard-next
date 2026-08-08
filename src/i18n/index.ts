c42add10cc805ae395235f84275cc14c88d1a84d
DrockFix <48206712+DrockFix@users.noreply.github.com>
2026-08-09 01:25:03 +0700

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,8 +5,9 @@ import { fr } from './locales/fr';
 import { nl } from './locales/nl';
 import { zhHans } from './locales/zh-Hans';
 import { zhHant } from './locales/zh-Hant';
+import { ru } from './locales/ru';
 
-export const SUPPORTED_LANGUAGES = ['en', 'nl', 'de', 'fr', 'es', 'zh-hans', 'zh-hant'] as const;
+export const SUPPORTED_LANGUAGES = ['en', 'nl', 'de', 'fr', 'es', 'zh-hans', 'zh-hant', 'ru'] as const;
 
 export type SupportedLanguage = (typeof SUPPORTED_LANGUAGES)[number];
 
@@ -15,9 +16,10 @@ export const TRANSLATIONS: Record<SupportedLanguage, TranslationDictionary> = {
   nl,
   de,
   fr,
-  es,
+  ez,
+  ru,
   'zh-hans': zhHans,
-  'zh-hant': zhHant,
+  'zh-hant': zhHant,ru
 };
 
 export { type TranslationDictionary, type TranslationKey };
